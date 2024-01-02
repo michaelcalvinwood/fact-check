c2468a4c59be9649ce1d1d23cb6295f90535a297
Michael Wood <michaelwood33311@icloud.com>
2024-01-02 10:08:58 -0500

    added initial infrastructure for /compare endpoint

--- fact-check.py
+++ fact-check.py
@@ -9,6 +9,25 @@ app = Flask(__name__)
 def helloWorld():
     return 'Hello World'
 
+@app.route('/compare', methods=["POST"])
+def compare():
+    data = request.get_json()
+    sources = data.get('sources', [])
+    content = data.get('content', '')
+
+    num_sources = len(sources)
+    content_length = len(content)
+
+    if (num_sources == 0):
+        return "Bad Command: missing sources", 400
+
+    if (content_length == 0):
+        return "Bad Command: missing content", 400
+    
+    return f"Num Sources: {len(sources)}"
+
+    return 'ok'
+
 if __name__ == '__main__':
     print('running the app')
     app.run(host=host, port=port)
